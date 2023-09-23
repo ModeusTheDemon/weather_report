5ba4224603ffd041adad52c05695838313ada49b
ModeusTheDemon <116836559+ModeusTheDemon@users.noreply.github.com>
2023-09-23 23:07:50 +0200

    Delete weather_project directory

--- weather_project/account_system/user_account.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, name: str, mail: str, login: str, password: str):
-        self.name = name
-        self.mail = mail
-        self.login = login
-        self.password = password
-    
